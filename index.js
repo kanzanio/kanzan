779a950b556184aa43cf4218ee627faed5a353cd
Phil Hawksworth <phawksworth@gmail.com>
2015-01-20 17:27:44 +0000

    adding commander structure and gitty

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-module.exports = function() {
-  console.log("Coming soon. For more information see http://kanzan.io");
+#! /usr/bin/env node
+
+var program = require('commander');
+var colors = require('colors');
+var kanzan = require("./lib");
+
+
+
+program
+  .command('init')
+  .description('Setup a new kanzan site.')
+  .action(function() {
+    kanzan.init.prompt();
+    kanzan.git.addKanzanRemote();
+  });
+
+program
+  .command('push')
+  .description('Push your HTML templates to kanzan.io')
+  .action(function() {
+    // kanzan.git.go();
+  });
+
+
+
+
+program.parse(process.argv);
+
+var validCommands = program.commands.map(function(cmd) {
+  return cmd._name;
+});
+
+if (!program.args.length) {
+  console.log('\n', 'kanzan'.cyan, 'http://kanzan.io');
+  program.help();
+} else if (validCommands.indexOf(process.argv[2]) === -1) {
+  console.log('Invalid argument:'.red, process.argv[2]);
+  console.log('kanzan:'.cyan, 'http://kanzan.io');
+  program.help();
 }
+
+
 
